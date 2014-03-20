51bdecd2fc1350f9864232d1d7d3f1c9b2b170fa
stx <stx@stx-VirtualBox.(none)>
2014-03-20 12:29:44 +0100

    first commit

---
+++ apps/webapp/webapp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+from flask import Flask        # Importation du module Flask
+
+webapp = Flask(__name__)        # Création d'une nouvelle application
+                                # Flask
+
+@webapp.route("/")    # Chemin depuis la racine du site
+                      # http://127.0.0.1:8080/webapp/ (serveur apache)
+                      # ou   http://127.0.0.1:5000 (lancé avec python webapp.py)
+         
+def hello():          # Fonction qui sera appelée lorsque
+                      # l'on accède au chemin indiqué dans    
+                      # @webapp.route("/")    
+
+     # Code renvoyé au navigateur                          
+    return "Hello World from the webapp app!"  
+    
+if __name__ == '__main__':
+    "Are we in the __main__ scope? Start test server."
+    webapp.run()
---
+++ wsgi/webapp.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0,'/home/stx/public_html/apps/webapp')
+from webapp import webapp as application
